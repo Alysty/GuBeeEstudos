318e799dfd1c0e51ea7ba49e25637a477e010eb9
Alysty <pppjw@protonmail.ch>
2021-05-05 15:03:23 -0300

    simple sum calculator using javaFX

--- JavaFX_Secao22/src/controllers/ViewController.java
+++ JavaFX_Secao22/src/controllers/ViewController.java
@@ -4,14 +4,28 @@ import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 
     @FXML
-    private Button bt;
-
+    private Button btCalculate;
     @FXML
-    public void onBtAction(){
-        Alerts.showAlert("Title", null,"This was a mistake", Alert.AlertType.WARNING);
+    private Label lbResult;
+    @FXML
+    private TextField txtFirstNumber;
+    @FXML
+    private TextField txtSecondNumber;
+    @FXML
+    public void onBtCalculateAction(){
+        try{
+            Double result = Double.parseDouble(txtFirstNumber.getText()) + Double.parseDouble(txtSecondNumber.getText()) ;
+            lbResult.setText(String.format("%.2f", result));
+        }catch(NumberFormatException e){
+            Alerts.showAlert("Error on calculation", null,"Please only use numbers", Alert.AlertType.WARNING);
+            System.out.println("ERROR"+ e +"ERROR");
+        }
+
     }
 }
