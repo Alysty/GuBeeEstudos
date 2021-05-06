07b17999d3a58867e5b41506fe75a9a62d910698
Alysty <pppjw@protonmail.ch>
2021-05-06 09:23:31 -0300

    initial commit for the first project of the course

---
+++ PROJETO_CRUD_Store_Secao23/src/controllers/ViewController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
