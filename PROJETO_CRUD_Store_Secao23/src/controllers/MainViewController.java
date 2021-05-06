f50d5565f8ebf82e72a6f82fad28c7f8df75bb4f
Alysty <pppjw@protonmail.ch>
2021-05-06 10:15:42 -0300

    MainView controller has been modified with attribuites and test methods for the taskbar and its items

--- PROJETO_CRUD_Store_Secao23/src/controllers/MainViewController.java
+++ PROJETO_CRUD_Store_Secao23/src/controllers/MainViewController.java
@@ -3,17 +3,33 @@ package controllers;
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
+    // Declaration of attributes
+    @FXML
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
 
-
+    //methods
+    @FXML
+    private void menuItemSellerAction(){
+        System.out.println("testing : menuItemSellerAction");
+    }
+    @FXML
+    private void menuItemDepartmentAction(){
+        System.out.println("testing : menuItemDepartmentAction");
+    }
+    @FXML
+    private void menuItemAboutAction(){
+        System.out.println("testing : menuItemAboutAction");
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
