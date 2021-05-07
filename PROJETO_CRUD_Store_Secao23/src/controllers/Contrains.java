8bfdfce76534dfe7c28e9462f9ede6f2cfbbf74a
Alysty <pppjw@protonmail.ch>
2021-05-07 09:55:21 -0300

    Added all the attributes and methods that are directly attached to the FXML for the DeparmentFormViewController

--- PROJETO_CRUD_Store_Secao23/src/controllers/Contrains.java
+++ PROJETO_CRUD_Store_Secao23/src/controllers/Contrains.java
@@ -4,5 +4,11 @@ package controllers;
 import javafx.scene.control.TextField;
 
 public class Contrains {
-
+    public static void setTextFieldMaxLength(TextField txt, int max) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && newValue.length() > max) {
+                txt.setText(oldValue);
+            }
+        });
+    }
 }
--- PROJETO_CRUD_Store_Secao23/src/controllers/DepartmentFormViewController.java
+++ PROJETO_CRUD_Store_Secao23/src/controllers/DepartmentFormViewController.java
@@ -1,4 +1,43 @@
 package controllers;
 
-public class DepartmentFormViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentFormViewController implements Initializable {
+    // Declaration of attributes attached to FXML
+    @FXML
+    private TextField textFieldID;
+    @FXML
+    private TextField textFieldName;
+    @FXML
+    private Label labelError;
+    @FXML
+    private Button buttonSave;
+    @FXML
+    private Button buttonCancel;
+    //Attributes not attached to FXML
+
+    //methods with direct attachment to the GUI
+    @FXML
+    public void buttonSaveAction(){
+        System.out.println("Test action for button buttonSaveAction");
+    }
+    @FXML
+    public void buttonCancelAction(){
+        System.out.println("Test action for button buttonCancelAction");
+    }
+    //methods not attached to FXML directly
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Contrains.setTextFieldMaxLength(textFieldName, 30);
+    }
 }
