8becd85afcb8e2c9568b8292097466dcf5e9a903
Alysty <pppjw@protonmail.ch>
2021-05-11 10:33:52 -0300

    Created entity order, changed name of entities to tb_CLASSNAME to prevent conflicts with SQL

--- PROJETO_Web_services_Secao25/src/main/java/com/web_services/projeto_web_services_secao25/entities/User.java
+++ PROJETO_Web_services_Secao25/src/main/java/com/web_services/projeto_web_services_secao25/entities/User.java
@@ -1,14 +1,14 @@
 package com.web_services.projeto_web_services_secao25.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -18,9 +18,12 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
-
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
     public User() {
     }
+
+
 
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
@@ -68,6 +71,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
