f644a929957479c3019fa35167249b467cc4b120
Alysty <pppjw@protonmail.ch>
2021-05-12 14:18:59 -0300

    Added config file for tests on the with the database

---
+++ PROJETO_Web_NoSql_Secao26/src/main/java/com/web_nosql/projeto_web_nosql_secao26/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.web_nosql.projeto_web_nosql_secao26.config;
+
+import com.web_nosql.projeto_web_nosql_secao26.domain.User;
+import com.web_nosql.projeto_web_nosql_secao26.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+    }
+}
