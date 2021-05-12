58c29ab0c3e10d3a66ff3b8af3e58732bca6c0a3
Alysty <pppjw@protonmail.ch>
2021-05-12 13:39:37 -0300

    Initial commit project web with nosql

---
+++ PROJETO_Web_NoSql_Secao26/src/main/java/com/web_nosql/projeto_web_nosql_secao26/ProjetoWebNoSqlSecao26Application.java
@@ -0,0 +1,13 @@
+package com.web_nosql.projeto_web_nosql_secao26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebNoSqlSecao26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoWebNoSqlSecao26Application.class, args);
+    }
+
+}
---
+++ PROJETO_Web_NoSql_Secao26/src/test/java/com/web_nosql/projeto_web_nosql_secao26/ProjetoWebNoSqlSecao26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_nosql.projeto_web_nosql_secao26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebNoSqlSecao26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
